93137db7c722981cfc2740ecd0d2725fd7635e31
Nina Hessell <nina.hessell@ppfa.org>
2022-11-17 16:25:12 -0500

    Add react router and nav bar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import './categories.styles.scss';
-import CategoriesContainer from './components/categories/categories.component';
-import CategoryItem from './components/category-item/category-item.component';
+import Home from './routes/home/home.component.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component.jsx';
+
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
 
   return (
-    <CategoriesContainer categories={categories}/>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
